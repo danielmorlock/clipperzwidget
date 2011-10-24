fdd0d00c5a0f3c96dea7c98f358bcc73c0ce993f
Daniel Morlock <daniel.morlock@gmail.com>
2011-10-24 11:08:08 +0000

    implement add_login()

--- widget/chrome/content/overlay.js
+++ widget/chrome/content/overlay.js
@@ -96,9 +96,9 @@ MochiKit.Base.update(ClipperzWidget.prototype, {
         document.getElementById("context-clipperzwidget").hidden = gContextMenu.onImage;
     },
     
-    'analyse_page': function(aEvent)
+    'analyse_page': function(e)
     {
-        var doc = aEvent.originalTarget; // doc is document that triggered the event  
+        //var doc = event.originalTarget; // doc is document that triggered the event  
         //var win = doc.defaultView; // win is the window for the doc  
     
         // test desired conditions and do something  
@@ -106,7 +106,36 @@ MochiKit.Base.update(ClipperzWidget.prototype, {
         // if (win != win.top) return; //only top window.  
         // if (win.frameElement) return; // skip iframes/frames  
         
-        // Match form actions
+        return this.find_forms(e.originalTarget);
+    },
+    
+    'analyse_pages': function()
+    {
+        var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]  
+                           .getService(Components.interfaces.nsIWindowMediator);  
+                                                          
+        var iter = wm.getEnumerator("navigator:browser");  
+
+        while(iter.hasMoreElements())
+        {
+            var gbrowser = iter.getNext().gBrowser;
+            for (var i = 0; i < gbrowser.browsers.length; i++)
+            {  
+                var b = gBrowser.getBrowserAtIndex(i);  
+                try
+                { 
+                    this.find_forms(b.contentDocument);
+                }
+                catch(msg)
+                {
+                    this.wwarning(msg);
+                }
+            }
+        }
+    },
+    
+    'find_forms': function(doc)
+    {
         var forms = doc.getElementsByTagName("form");
         for(var i = 0; i < forms.length; i ++)
         {
@@ -114,11 +143,15 @@ MochiKit.Base.update(ClipperzWidget.prototype, {
             {
                 if(this.form_data[j]["attributes"]["action"] == forms[i].action)
                 {
-                    this.info("found registered form action \"" + this.form_data[j]["attributes"]["action"] + "\"");
+                    this.info("found registered form action \"" + 
+                        this.form_data[j]["attributes"]["action"] + "\"");
+                    
                     return this.set_form_data(j, forms[i]);
                 }
             }
         }
+        
+        return null;
     },
     
     'set_form_data': function(reference, form)
@@ -169,7 +202,7 @@ MochiKit.Base.update(ClipperzWidget.prototype, {
         result.addCallback(MochiKit.Base.method(this.user, 'loadRecords'));
         result.addCallback(MochiKit.Base.method(this.user, 'loadDirectLogins'));
         result.addCallback(MochiKit.Base.method(this, function() 
-            { this.info("user logged in successfully"); }));
+            {this.info("user logged in successfully");}));
         
         // Load available direct logins and keep form actions!
         result.addCallback(MochiKit.Base.method(this, "load_direct_logins"));
@@ -189,6 +222,9 @@ MochiKit.Base.update(ClipperzWidget.prototype, {
     {        
         try
         {
+            // Reset form data
+            this.form_data = {};
+            
             var result = new MochiKit.Async.Deferred();
             var login_refs = MochiKit.Base.values(this.user.directLoginReferences());
 
@@ -210,7 +246,7 @@ MochiKit.Base.update(ClipperzWidget.prototype, {
 
                 }), i, login_refs[i].record(), login_refs[i].reference());
             }
-            
+            result.addCallback(MochiKit.Base.method(this, 'analyse_pages'));
             result.addCallback(MochiKit.Base.method(this, function()
             {
                 this.info("direct logins loaded"); 
@@ -228,6 +264,11 @@ MochiKit.Base.update(ClipperzWidget.prototype, {
         {
             this.error(error);
         }
+    },
+    
+    'debug': function(msg)
+    {
+        this.log("debug", msg);
     },
     
     'info': function(msg)
@@ -282,7 +323,59 @@ MochiKit.Base.update(ClipperzWidget.prototype, {
     
     'add_login': function()
     {
-        dump("add_login");
+        this.debug("add_login");
+        
+        try
+        {
+            /*
+            var json = '{"page": {"title": "Clipperz - online password manager - debug"}, '+
+                       '"form": {"attributes": {"action": "http://localhost:81/workspace/clipperz/target/php/test_login.php",'+
+                       '"method": "post"},'+
+                       '"inputs": [{"type": "text",'+
+                       '"name": "username",'+
+                       '"value": "da"},'+
+                       '{"type": "password",'+
+                       '"name": "password",'+
+                       '"value": "da"}]},'+
+                       '"version": "0.2.3"}';
+                   
+            var configuration = 
+                Clipperz.PM.BookmarkletProcessor.checkBookmarkletConfiguration(json, null, 
+                    MochiKit.Base.method(this, function() {this.error("invalid json configuration");}));
+            */                    
+                    
+            var configuration = {};
+            configuration.page = pageParameters();
+            configuration.form = formParameters(findLoginForm(content.document, 0));
+            configuration.version = "0.2.3";
+            this.debug("got a configuration: " + configuration);            
+
+            var record = 
+                Clipperz.PM.BookmarkletProcessor.createRecordFromBookmarkletConfiguration(this.user, configuration);
+            this.debug("got a record: " + record);
+
+            var result = new MochiKit.Async.Deferred();            
+            result.addCallback(MochiKit.Base.method(record, 'saveChanges'));
+            result.addCallback(MochiKit.Base.method(this, "load_direct_logins"));
+            
+            result.addCallback(MochiKit.Base.method(this, function() 
+            {
+                this.info("record saved successfully");
+            }));
+            
+            result.addErrback(MochiKit.Base.method(this, function(msg) 
+            {
+                this.error("could not save record: " + msg);
+            }));
+            
+            result.callback();
+            
+            return result;
+        }
+        catch(msg)
+        {
+            this.error("error while adding result: " + msg);
+        }
     },
     
     'copy_username': function()
